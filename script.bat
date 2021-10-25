3d47276998c92ea5da076ebac41d17f012344117
sebateso90 <sebastian.tesolin@yahoo.com.ar>
2021-10-24 23:26:36 -0300

    se modifica el script con segunda.exe

--- script.bat
+++ script.bat
@@ -4,9 +4,9 @@ C:\GnuWin32\bin\bison -dyv sintactico.y
 echo "generando archivo flex.."
 C:\GnuWin32\bin\flex Lexico.l
 echo "compilando el archivo flex..se crea el compilado.exe"
-C:\MinGW\bin\gcc.exe y.tab.c lex.yy.c -o Primera.exe
+C:\MinGW\bin\gcc.exe y.tab.c lex.yy.c -o Segunda.exe
 echo "analizador lexico sobre pruebas.txt"
-type pruebas.txt | Primera.exe
+type pruebas.txt | Segunda.exe
 echo "se borran archivos demas.."
 del y.tab.c
 del y.tab.h
